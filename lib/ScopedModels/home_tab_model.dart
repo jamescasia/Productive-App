797be7a6ce9998cc14d80288123adb118ff6d7ac
James <wayperayp@gmail.com>
2020-03-11 20:34:53 +0800

    added hometabmodel

---
+++ lib/ScopedModels/home_tab_model.dart
@@ -0,0 +1,17 @@
+import 'package:ProductiveApp/ScopedModels/app_model.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class HomeModel extends Model {
+  AppModel appModel;
+  HomeModel(this.appModel);
+
+  
+
+
+
+
+
+
+
+}
+
